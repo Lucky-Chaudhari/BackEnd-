3ff28ce38c305c5906b28df725b2b7beac37602e
Lucky Chaudhari <luckychaudhari014@gmail.com>
2024-12-13 14:45:25 +0530

    completed the Blog app

--- Blog App/controllers/likeController.js
+++ Blog App/controllers/likeController.js
@@ -1,6 +1,49 @@
 
 
+//import models
 
-   exports.dummyLike = (req,res)=>{
-  res.send("This is Your Dummy Page");
+const Like = require("../models/likeModel");
+const Post = require("../models/postModel");
+
+//Like a Post
+
+exports.likePost = async (req,res) => {
+  try {
+    const {post, user}=req.body
+    const like = new Like({
+      post, user,
+    });
+    const savedLike=like.save();
+
+    //update   the post collection basis on this
+    const updatedPost = await Post.findByIdAndUpdate(post,{$push:{likes:savedLike._id}},{new:true}).populate("likes").exec();
+
+     res.json({
+      post:updatedPost
+     })
+  } catch (error) {
+return res.status(400).json({
+   error:"Error while liking post",
+});
+  }
+}
+// Unlike a Post
+
+exports.unlikePost = async(req,res) =>{
+
+try {
+
+  const{post, like}=req.body;
+  //find and delete the collection me se
+  const deletedLike = await Loke.findOneAndDelete({post:post, _id:like});
+
+  //update the post collection
+  const updatedPost= await Post.find.findOneAndUpdate(post,{$pull:{likes:deletedLike._id}},{new:true})
+
+} catch (error) {
+  return res.status(400).json({
+    error:"Error while Unliking post",
+ });
+}
+
 };
--- Blog App/routes/blog.js
+++ Blog App/routes/blog.js
@@ -4,12 +4,17 @@ const router = express.Router();
 
 //Import Controller
 
-const { dummyLike } = require("../controllers/likeController")
+const { likePost,unlikePost } = require("../controllers/likeController")
 const {createComment} = require("../controllers/commentController")
+const {createPost,getAllPosts} = require("../controllers/postController")
 
 //Mapping Create
-router.get("/dummyrouter",dummyLike);
-router.post("comments/create",createComment);
+
+router.post("/comments/create",createComment);
+router.post("/posts/create",createPost)
+router.get("/posts",getAllPosts);
+router.post("/likes/like",likePost);
+router.post("/likes/unlike",unlikePost);
 
 
 
