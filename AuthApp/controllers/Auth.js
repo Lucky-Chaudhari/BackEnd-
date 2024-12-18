154fc4ee207e628a9c3af3d3b122c26dde17e0f0
Lucky Chaudhari <luckychaudhari014@gmail.com>
2024-12-18 15:44:59 +0530

    this Authentication Practice

---
+++ AuthApp/controllers/Auth.js
@@ -0,0 +1,52 @@
+const bcrypt= require("bcrypt")
+const User= require("../models/User")
+
+
+
+
+
+//signup route handler
+exports.signup = async(req,res)=>{
+
+  try {
+    //get data
+    const{name,email,password,role}=req.body;
+    //check if user already exist
+    const existingUser=await User.findOne({email});
+
+    if(existingUser){
+      return res.status(400).json({
+        success:false,
+        message:"User Already Exists",
+      })
+    }
+//secure Password
+
+let hashedPassword;
+try{
+  hashedPassword=await bcrypt.hash(password,10);
+}
+catch(err){
+  return res.status(500).json({
+    success:false,
+    message:"Error in hashing Password",
+  })
+
+}
+//create entry for user
+const user = await User.create({
+  name,email,password:hashedPassword,role
+})
+returnres.status(200).json({
+  success:false,
+  message:"User Create Successfully",
+});
+
+  } catch(error) {
+console.error(error);
+return res.status(500).json({
+  success:false,
+  message:'User cannot be registered, Please try again later',
+});
+  }
+}
---
+++ AuthApp/index.js
@@ -0,0 +1,23 @@
+const express= require("express");
+const app=express();
+
+
+require('dotenv').config();
+
+const PORT = process.env.PORT ||4000;
+
+app.use(express.json());
+
+
+require("./config/database").DBconnect();
+
+//route import and mount
+
+const user= require('./routes/user');
+app.use("/api/v1",user);
+
+
+
+app.listen(PORT,()=>{
+  console.log(`App is listenine at ${PORT}`);
+})
